601c7776879efc45dadc966b19b2ec29d80ff814
nyra williams <L00162304@student.lyit.ie>
2021-05-12 15:27:22 -0400

    commit

--- CA1.R
+++ CA1.R
@@ -29,10 +29,75 @@ str(world_happiness_data)
 install.packages("psych")
 library(psych)
 
-# Look at the correlation between both variables 
+# Using the default plot() option first to examine correlations between variables
 # Evaluate the strength of the relationship
 # Determine if the relationship is negative or positive
 attach(world_happiness_data)
-plot(Positive.affect, Freedom.to.make.life.choices, pch = 19, col = "red")
+plot(Freedom.to.make.life.choices, Positive.affect, pch = 19, col = "red")
 
+pairs(world_happiness_data, labels = colnames(world_happiness_data), main = "World Happiness dataset correlation plot")
+
+pairs.panels(world_happiness_data, 
+             smooth = FALSE, # If TRUE, draws loess smooths  
+             scale = FALSE, # If TRUE, scales the correlation text font  
+             density = TRUE, # If TRUE, adds density plots and histograms  
+             ellipses = FALSE, # If TRUE, draws ellipses   
+             method = "spearman",# Correlation method (also "pearson" or "kendall") 
+             pch = 21, # pch symbol   
+             lm = FALSE, # If TRUE, plots linear fit rather than the LOESS (smoothed) fit 
+             cor = TRUE, # If TRUE, reports correlations
+             jiggle = FALSE, # If TRUE, data points are jittered  
+             factor = 2, # Jittering factor  
+             hist.col = 4, # Histograms color   
+             stars = TRUE,
+             ci = TRUE) # If TRUE, adds confidence intervals 
+
+# Check linearity 
+attach(world_happiness_data)
+plot(Positive.affect,
+     Freedom.to.make.life.choices,
+     pch = "19",
+     col = "red", 
+     main = "Comparison of Positive Affect and Freedom to Make Life Choices",
+     xlab = " Positive Affect", ylab = "Freedom to Make Life Choices")
+
+# Select the appropriate test
+# Check whether the data is normally distributed or not
+
+# Quantile-quantile plot allows us to check if the data is distributed normally
+# Compare the quantiles of both samples 
+
+attach(world_happiness_data)
+qqnorm(Positive.affect)
+
+# This code creates a line that represents normal distribution
+qqline(Positive.affect, col = "red")
+
+# Using a QQ plot to check for normality
+# qqnorm function plots your sample against a normal distribution
+with(world_happiness_data, {
+  qqnorm(Positive.affect[Freedom.to.make.life.choices], 
+         main = "Inactive")
+})
+
+# Use this code to test for normality using the Shapiro-Wilks test
+# Normality test for the variable positive affect
+
+normality_test <- shapiro.test(world_happiness_data$Positive.affect)
+normality_test$p.value
+
+# p-value tells us the chances that the sample comes from a normal distribution 
+# The p-value is lower than 0.05 so the positive affect variable is not normally distributed
+
+# Use this code to test for normality using the Shapiro-Wilks test
+# Normality test for the variable freedom to make life choices
+
+normality_test <- shapiro.test(world_happiness_data$Freedom.to.make.life.choices)
+normality_test$p.value
+
+# The p-value is lower than 0.05 so the freedom to make life choices variable is not normally distributed
+
+# The p-value for the dependent variable "positive affect" indicates that the data is not normally distributed so we need to use a non-parametric test
+# After looking at the plots and the p-value, I am examining a dependent continuous variable (positive affect) and an independent continuous variable (freedom to make life choices)
+# So I used the Spearman’s Correlation Coefficient test
 
